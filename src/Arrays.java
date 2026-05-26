480351df0f22127d6884a37d39003d8be401d06f
rahul18kumar <rk4955528@gmail.com>
2026-05-26 16:48:07 +0530

    Start with the array

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays {
+
+    public static void main(){
+        // Declaration ->  name only
+        int arr[];
+        //Allocation -> memory only
+        arr = new int[4];
+        //Initialisation -> values inserted
+        int num[]={3,2,4,24,2,4,6};
+        //Accessing Array
+        System.out.println("4th Element of array num is "+ num[4]);
+    }
+}
