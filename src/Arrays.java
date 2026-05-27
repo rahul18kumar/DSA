642ad9b74ef8e0624c743e5a6a226ad224052c50
rahul18kumar <rk4955528@gmail.com>
2026-05-28 01:26:06 +0530

    Completing basics of array

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,8 +1,75 @@
+import jdk.jshell.DeclarationSnippet;
+
 import java.util.Scanner;
 public class Arrays {
 
     public static void main(){
 
+
+        //Start with the 2-D Array
+//
+////        Declaration
+//        int arr[][];
+////        Allocation
+//        arr = new int[3][3];
+////        Initialisation
+//        int brr[][] = {{1,2,3,4},
+//                        {5,6,7,8,},
+//                        {9,10,11,12}
+//                        };
+//        for (int row = 0; row <=brr.length-1 ; row++) {
+//            for (int col = 0; col <=brr[row].length-1 ; col++) {
+//                System.out.print(brr[row][col]+" ");
+//            }
+//            System.out.println(" ");
+//        }
+
+//        //Input in 2d Array
+//        int [][] arr = new int[3][3];
+//        Scanner input = new Scanner(System.in);
+//        for (int row = 0; row <=arr.length-1 ; row++) {
+//            for (int col = 0; col <=arr[row].length-1 ; col++) {
+//                System.out.println("Enter the value at "+ row +" "+ col);
+//                arr[row][col]= input.nextInt();
+//
+//            }
+//            System.out.println(" ");
+//        }
+//
+//        //Output
+//        System.out.println("Full Array are : ");
+//        for (int i = 0; i <=arr.length-1 ; i++) {
+//            for (int j = 0; j <=arr[i].length-1 ; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println(" ");
+//        }
+
+        //Q1. Print sum of all elements in the ad array
+//        int arr[][]={{1,2,3},{4,5,6},{8,9,10}};
+//        int sum =0;
+//        for (int i = 0; i <=arr.length-1 ; i++) {
+//            for (int j = 0; j <=arr[i].length-1 ; j++) {
+//                sum=sum+arr[i][j];
+//            }
+//        }
+//        System.out.println("Final sum of array elements "+ sum);
+
+//        Q2.Find Max element in array.
+//        int arr[][]={{2,45,54},{43,278,65}};
+//        int maxvalue = arr[0][0];
+//        for (int i = 0; i <= arr.length-1 ; i++) {
+//            for (int j = 0; j <=arr[i].length-1 ; j++) {
+//                if(arr[i][j]>maxvalue){
+//                    maxvalue=arr[i][j];
+//                }
+//            }
+//        }
+//        System.out.println("Maximum value in this array is"+ maxvalue);
+
+
+
+        //1D Array
 //        Q1. Sum all the elements of an array provided.
 //        int summ []={1,2,2,4,9};
 //        int p = summ.length;
