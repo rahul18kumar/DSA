7afcc92d09f3f4336e6dce4e895544a1ffcae2c8
rahul18kumar <rk4955528@gmail.com>
2026-05-29 01:19:18 +0530

    start with strings

---
+++ src/StringBasics.java
@@ -0,0 +1,93 @@
+import javax.lang.model.element.NestingKind;
+import java.util.Scanner;
+
+public class StringBasics {
+
+    static void main() {
+//
+//        String name = "My name is rahul";   //Delimeter is space here
+//        String [] words = name.split(" ");
+////        print via for each loop
+//        for (String str: words){
+//            System.out.println(str);
+//        }
+
+//
+//        String name = "RahuL";
+//        char [] crr = name.toCharArray();
+//
+//     //   for each character value we are using for each loop here
+//        for (char out:crr){
+//            System.out.println(out);
+//        }
+
+//        String name = "my name is rahul";
+//        System.out.println(name.startsWith("my "));
+//        System.out.println(name.endsWith("hul"));
+
+//        int num = 5123;
+//
+//        String str = String.valueOf(num);
+//
+//        System.out.println(num + 1);
+//
+//        System.out.println(str + 1);
+
+
+//String str = "My Name is Rahul Kumar";
+
+//0 -> M
+//1 -> y
+//2 -> space and so on
+
+    //beginIndex -> 3 -> inclusive
+    //endIndex -> 6 -> exclusive
+
+    //System.out.println(str.substring(3, 7));
+
+    //System.out.println(str.contains("Lover"));
+
+        //Empty-> it means length is 0
+        //blank -> it means empty or some blank spaces.
+        //empty -> length = 0
+        //blank -> empty or just spaces in string
+
+//        String str = "";
+//
+//        System.out.println(str.length());
+//        System.out.println(str.isEmpty());
+//        System.out.println(str.isBlank());
+//        String name = "   Love   ";
+//        System.out.println(name.length());
+//        name = name.trim();
+//        System.out.println(name.length());
+//        String name1 = "Love";
+//        System.out.println(name1.toUpperCase());
+//        String str1 = "LOVE";
+//        System.out.println(name1.toLowerCase());
+
+        //String common methods
+//        String str = "Rahul";
+//        System.out.println(str.length());
+//        System.out.println(str.charAt(1));
+//        String str1 = "RAhul";
+//        System.out.println(str.equals(str1));   //false
+//        System.out.println(str.equalsIgnoreCase(str1));     //true
+
+//        Scanner sc = new Scanner(System.in);
+////        System.out.println("Provide the string");
+////        String str= sc.next();  //single word
+////        System.out.println("Entered string "+ str);
+////        System.out.println("Enter second times");
+//        System.out.println("Enter string");
+//        String str1 = sc.nextLine();
+//        System.out.println("Value 2 " + str1);
+//        String str ="Rahul";
+//        //or
+//        String str1 = new String("Kumar");
+//
+//        System.out.println(str +" "+ str1);
+//        System.out.println(str.length());
+//        System.out.println(str.charAt(0));
+    }
+}
